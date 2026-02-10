5215226a383d6d9d956ba63c1a1159fa8c8d4016
Unc Degen <ilyakomoloskogv@hotmail.com>
2026-02-10 16:36:16 +0200

    Update NFTMarketplaceRoyalties.sol

--- contracts/NFTMarketplaceRoyalties.sol
+++ contracts/NFTMarketplaceRoyalties.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-/* 
-  Educational NFT:
-
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
