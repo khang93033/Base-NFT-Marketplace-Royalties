33e2053bf40202ca5c4bc45fee7e1488b07b2162
Unc Degen <ilyakomoloskogv@hotmail.com>
2026-02-10 16:36:09 +0200

    Update NFTMarketplaceRoyalties.sol

--- contracts/NFTMarketplaceRoyalties.sol
+++ contracts/NFTMarketplaceRoyalties.sol
@@ -3,9 +3,6 @@ pragma solidity ^0.8.20;
 
 /* 
   Educational NFT:
-  - default royalty
-  - per-token royalty override
-*/ 
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
