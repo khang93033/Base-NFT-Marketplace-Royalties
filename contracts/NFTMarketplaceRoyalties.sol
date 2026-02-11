9140c4558e4b5f28aa9cd82dae19f6cb9d91e13e
Unc Degen <ilyakomoloskogv@hotmail.com>
2026-02-11 16:02:09 +0200

    Update NFTMarketplaceRoyalties.sol

--- contracts/NFTMarketplaceRoyalties.sol
+++ contracts/NFTMarketplaceRoyalties.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/common/ERC2981.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFTMarketplaceRoyalties is ERC721URIStorage, ERC2981, Ownable {
     uint256 public nextTokenId = 1;
