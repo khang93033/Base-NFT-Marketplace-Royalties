298269f21ec1d90ddcc18430885e60b88bc312b4
Unc Degen <ilyakomoloskogv@hotmail.com>
2026-02-24 12:07:55 +0200

    Update TestToken.sol

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -5,8 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor() ERC20("Test Token", "TEST") {
-        _mint(msg.sender, 1000000 * 10**18); // 1 million tokens
+    constructor() ERC20("Test Token", "TEST") {// 1 million tokens
     }
 
     function mint(address to, uint256 amount) external {
