715ff152339226f11a2ac573845ca671d478aadd
Unc Degen <ilyakomoloskogv@hotmail.com>
2026-03-17 16:45:38 +0200

    Update NFTMarketplaceRoyalties.sol

--- contracts/NFTMarketplaceRoyalties.sol
+++ contracts/NFTMarketplaceRoyalties.sol
@@ -8,35 +8,69 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract NFTMarketplaceRoyalties is ERC721URIStorage, ERC2981, Ownable {
     uint256 public nextTokenId = 1;
 
-    event DefaultRoyaltySet(address receiver, uint96 feeNumerator);
     event Minted(address indexed to, uint256 indexed tokenId, string uri);
+    event Burned(uint256 indexed tokenId);
+    event DefaultRoyaltySet(address indexed receiver, uint96 feeNumerator);
+    event TokenRoyaltySet(uint256 indexed tokenId, address indexed receiver, uint96 feeNumerator);
+    event TokenRoyaltyReset(uint256 indexed tokenId);
 
     constructor(address defaultReceiver, uint96 defaultFee) ERC721("BaseNFT", "BNFT") Ownable(msg.sender) {
         require(defaultReceiver != address(0), "receiver=0");
+        require(defaultFee <= 2000, "too high");
+
         _setDefaultRoyalty(defaultReceiver, defaultFee);
         emit DefaultRoyaltySet(defaultReceiver, defaultFee);
     }
 
+    function mint(address to, string calldata uri) external returns (uint256 tokenId) {
+        require(to != address(0), "to=0");
+
+        tokenId = nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+
+        emit Minted(to, tokenId, uri);
+    }
+
+    function burn(uint256 tokenId) external {
+        require(_isApprovedOrOwner(msg.sender, tokenId), "not approved");
+
+        _burn(tokenId);
+        _resetTokenRoyalty(tokenId);
+
+        emit Burned(tokenId);
+    }
 
     function setDefaultRoyalty(address receiver, uint96 feeNumerator) external onlyOwner {
         require(receiver != address(0), "receiver=0");
         require(feeNumerator <= 2000, "too high");
+
         _setDefaultRoyalty(receiver, feeNumerator);
         emit DefaultRoyaltySet(receiver, feeNumerator);
     }
 
-    function mint(address to, string calldata uri) external returns (uint256 tokenId) {
-        require(to != address(0), "to=0");
-        tokenId = nextTokenId++;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-        emit Minted(to, tokenId, uri);
+    function setTokenRoyalty(uint256 tokenId, address receiver, uint96 feeNumerator) external onlyOwner {
+        require(_exists(tokenId), "no token");
+        require(receiver != address(0), "receiver=0");
+        require(feeNumerator <= 2000, "too high");
+
+        _setTokenRoyalty(tokenId, receiver, feeNumerator);
+        emit TokenRoyaltySet(tokenId, receiver, feeNumerator);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721URIStorage, ERC2981) returns (bool) {
-        return super.supportsInterface(interfaceId);
-    }
     function resetTokenRoyalty(uint256 tokenId) external onlyOwner {
-    _resetTokenRoyalty(tokenId);
+        require(_exists(tokenId), "no token");
+
+        _resetTokenRoyalty(tokenId);
+        emit TokenRoyaltyReset(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721URIStorage, ERC2981)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
